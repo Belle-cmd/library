76ed5db798d871e4c7d640047132d4dd0be93538
BelleProjects <bautistadana55@gmail.com>
2023-07-01 20:51:00 -0600

    Draw book into webpage after new book creation

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 'use strict';
 
+
+
+// Retrieve elements form html document and attach functions
+const newBookwindow = document.getElementById("book-creation-wrapper");
+const addBookBtn = document.getElementById("add-btn");
+const addBookCloseBtn = document.getElementById("newbook-close");
+const newBookSubmitBtn = document.getElementById("book-creation");
+const mainElement = document.getElementsByTagName("main")[0];
+
+
+
+// At start of webpage...
+document.addEventListener("DOMContentLoaded", function() {
+    cardMode();
+  });
+
+
+// Objects in the webpage
 const library = [];  // store all books regardless of their list and status
 const completedList = [];  // store finished books
 const incompleteList = [];  // store books not yet finished
@@ -10,20 +28,55 @@ function Book(title, author, status) {
     this.status = status;
 }
 
-// populate library with dummy books
+// Populate library with dummy books
 const book1 = new Book("Feeling Good", "David D. Burns", false);
 const book3 = new Book("Frankenstein", "Mary Shelley", true);
 const book2 = new Book("The Picture of Dorian Gray", "Oscar Wilde", false);
 const book4 = new Book("The Power of Habit", "Charles Duhigg", true);
 const book5 = new Book("Dracula", "Bram Stoker", true);
-library.push(book1, book2, book3, book4, book5);
+const book6 = new Book("C Programming Language", "Kerninghan Ritchie", false);
+library.push(book1, book2, book3, book4, book5, book6);
 console.log(library);
 
 
-// actions executed in web page
 
+// Functions executed in web page
 
-function cardMode() {}
+function cardMode() {
+    library.forEach(item => {
+        const markup = `
+            <div class="card-img">
+                <div class="card-options">
+                    <button class="card-edit"></button>
+                    <button class="card-close"></button>
+                </div>
+                <button class="card-status"></button>
+                <img src="/images/default-book.svg" alt="">
+            </div>
+            <p class="card-title">${item.title}</p>
+            <p class="card-author">${item.author}</p>
+        `;
+
+        const newChild = document.createElement("div");
+        newChild.classList.add("card");
+        newChild.innerHTML = markup;
+
+        // retrieve the button with the card-status class and add the classes that change
+        // its button's color based on the user's reading progress (complete/incomplete)
+        const cardStatusElem = newChild.querySelector(".card-status");
+        if (item.status) {
+            // book finished reading
+            cardStatusElem.classList.add("complete");
+            cardStatusElem.classList.remove("incomplete");
+        } else {
+            //  book not finished reading
+            cardStatusElem.classList.remove("complete");
+            cardStatusElem.classList.add("incomplete");
+        }
+
+        mainElement.appendChild(newChild);
+    });
+}
 
 function listMode() {}
 
@@ -56,11 +109,8 @@ function removeBookFromLibrary() {}
 function editBookInLibrary() {}
 
 
-// Retrieve elements form html document and attach functions
-const newBookwindow = document.getElementById("book-creation-wrapper");
-const addBookBtn = document.getElementById("add-btn");
-const addBookCloseBtn = document.getElementById("newbook-close");
-const newBookSubmitBtn = document.getElementById("book-creation");
+
+// Event listeners triggered by users
 
 addBookBtn.addEventListener("click", () => {
     // Enable new-book-window to appear and disappear at button press
@@ -79,10 +129,14 @@ addBookCloseBtn.addEventListener("click", () => {
 newBookSubmitBtn.addEventListener("submit", (event) => {
     event.preventDefault();  // prevents page refresh at form submit
 
-    // retrieve form data
+    // Retrieve form data
     const title = document.getElementById("newbook-title").value;
     const author = document.getElementById("newbook-author").value;
     const status = document.getElementById("newbook-status").checked;
 
     addBookToLibrary(title, author, status);
+
+    mainElement.replaceChildren();  // remove all of main's children
+    cardMode();
+    newBookwindow.style.display = "none";  // hide the book creation window
 });
